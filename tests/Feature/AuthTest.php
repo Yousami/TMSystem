79cf17b4b70f3a7e73cd25150346cfb5612c4057
Yousami <11810476+Yousami@users.noreply.github.com>
2025-10-01 15:05:42 -0700

    test: add initial auth tests

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,12 @@
+<?php
+
+it('can register a user', function () {
+    $response = $this->post('/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
+
+    $response->assertStatus(302); // Redirect after register
+});
