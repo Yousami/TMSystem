6924f3fa2856955cc81fd222f5460b6f783f1cb6
Yousami <11810476+Yousami@users.noreply.github.com>
2025-10-01 14:56:24 -0700

    setup Swagger for API documentation

--- Modules/Base/app/Http/Controllers/BaseController.php
+++ Modules/Base/app/Http/Controllers/BaseController.php
@@ -7,6 +7,17 @@ use Illuminate\View\View;
 
 class BaseController extends Controller
 {
+        /**
+ * @OA\Get(
+ *     path="/api/users",
+ *     summary="Get list of users",
+ *     tags={"Users"},
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation"
+ *     )
+ * )
+ */
     public function index(): View
     {
         return view('base::dashboard');
--- Modules/Profile/app/Http/Controllers/ProfileController.php
+++ Modules/Profile/app/Http/Controllers/ProfileController.php
@@ -13,6 +13,17 @@ use Modules\Profile\Http\Requests\ProfileUpdateRequest;
 class ProfileController extends Controller
 {
     /**
+ * @OA\Get(
+ *     path="/api/users",
+ *     summary="Get list of users",
+ *     tags={"Users"},
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful operation"
+ *     )
+ * )
+ */
+    /**
      * Display the user's profile form.
      */
     public function edit(Request $request): View
